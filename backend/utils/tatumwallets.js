35625f2de3aa0bbadb592f8f54df35fde2497624
eze levi <ezelevi7@gmail.com>
2025-07-30 14:33:37 -0700

    Add CORS support for Vercel frontend

--- backend/utils/tatumwallets.js
+++ backend/utils/tatumwallets.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const API_KEY = process.env.TATUM_API_KEY;
+const axiosRetry = require('axios-retry');
 
 const headers = {
   'x-api-key': API_KEY,
@@ -37,6 +38,12 @@ async function getDepositAddress(chain, xpub, index = 0) {
     throw error;
   }
 }
+
+axiosRetry(axios, {
+  retries: 3,
+  retryDelay: axiosRetry.exponentialDelay,
+});
+
 
 module.exports = {
   generateWallets,
