b6494f06bff75942c28d9e97ffbea9938f07b47c
eze levi <ezelevi7@gmail.com>
2026-06-26 20:47:40 +0100

    Fix

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,19 +1,13 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
-  await transporter.sendMail({
-    from: `"Security Team" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: process.env.EMAIL_FROM,
     to,
     subject,
-    html
+    html,
   });
 };
 
